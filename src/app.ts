17724c53af1b0c5805f1bca4232682f5ddd1f46f
Raul <raul_cosmin_26@yahoo.com>
2025-03-19 23:10:35 +0200

    DOM Element selection & UUP rendering.

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,30 @@
 // Code goes here!
 console.log("works!");
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    // this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!; // type casting
+
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement; // type casting - same as above
+
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const projectInput = new ProjectInput();
